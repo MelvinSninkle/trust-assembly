ebc2d3f20e0867bd77e98595133f3348fd782c67
Daniel Williams <dwilliams@calance.com>
2024-11-28 12:43:12 -0700

    somre renaming

--- back/Scrape_Headlines/main.cs
+++ back/Scrape_Headlines/main.cs
@@ -3,11 +3,15 @@ using Scrape_Headlines.Utilities;
 
 namespace Scrape_Headlines
 {
-    internal class Program
+    internal class main
     {
         static void Main(string[] args)
         {
             Log.Info("Started");
+
+            // make sure playwright is installed
+            // Joe knows all: https://forum.linqpad.net/discussion/2710/using-playwright-in-linqpad
+            Microsoft.Playwright.Program.Main(new[] { "install" }); // Install Playwright if not already installed
 
             ///var site = new New_York_Times();
             //TODO: wrl and all shoudl just use playwright
